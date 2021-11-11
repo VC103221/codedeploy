47a0598b76ee9d4218bee223755628f8afcf69fa
VC103221 <26527236+VC103221@users.noreply.github.com>
2021-11-11 11:59:02 -0500

    Delete test-injections.js

--- test-injections.js
+++
@@ -1,9 +0,0 @@
-const name = "GW_NONPROFIT_NAME;
-const street = "GW_NONPROFIT_STREET";
-
-function assign_injections() {
-let selection = document.getElementById("mainHeader");
-selection.setAttribute("data-inject", name);
-let selection = document.getElementById("subPara");
-selection.setAttribute("data-inject", street);
-}
