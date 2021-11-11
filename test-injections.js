adf529668f058b0ea605dac8d203bab791454290
VC103221 <26527236+VC103221@users.noreply.github.com>
2021-11-11 11:41:16 -0500

    Create test-injections.js

---
+++ test-injections.js
@@ -0,0 +1,7 @@
+const name = "GW_NONPROFIT_NAME;
+const street = "GW_NONPROFIT_STREET";
+
+let selection = document.getElementById("mainHeader");
+selection.setAttribute("data-inject", name);
+let selection = document.getElementById("subPara");
+selection.setAttribute("data-inject", street);
